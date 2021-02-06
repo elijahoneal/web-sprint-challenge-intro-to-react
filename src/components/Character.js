a7cc78e536535f5f37125f1b522bdc0a4b2f9aea
Elijah <elijahoneal@yahoo.com>
2021-02-06 12:26:06 -0500

    Styled Character Cards

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,29 +1,68 @@
 // Write your Character component here
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+
+const Card = styled.div`
+display:flex;
+flex-flow: row wrap;
+justify-content: space-evenly;
+width:90%;
+margin: 2rem auto;
+
+`
+const CharStyle = styled.section`
+    background-color: rgba( 225 , 225 , 225 , 0.75 );
+    width: 30%;
+    margin: 1rem auto;
+    h3 {
+        color: #672F14;
+    }
+    section {
+        color: 
+        text-align : center;
+        ul {
+
+            margin: 0;
+            padding: 0;
+        }
+        li {
+            list-style: none;
+            line-height:1.5;
+        }
+
+    }
+
+`
 
 
 const Character = props => {
     const characters = props.people;
+   
+   
 
     return (
-        characters.map( character => {
+        <Card>
+       { characters.map( character => {
             return (
-                <section>
+                <CharStyle>
                     <h3>Name: {character.name}</h3>
-                    <p>Birth: {character.birth_year}</p>
-                    <p>Gender: {character.gender}</p>
-                    <p>Height: {character.height}</p>
-                    <p>Weight: {character.mass}</p>
-                    <ul> Film Appearances:
-                        {character.films.map( film => {
-                       return <li key = {Math.random()}>{film}</li>
-                    })}
+                    <section>
+                        <p>Birth: {character.birth_year}</p>
+                        <p>Gender: {character.gender}</p>
+                        <p>Height: {character.height}</p>
+                        <p>Weight: {character.mass}</p>
+                        <ul> Film Appearances:
+                            {character.films.map( film => {
+                        return <li key = {Math.random()}>{film}</li>
+                        })}
                     </ul>
-                </section>
+                    </section>
+                </CharStyle>
             )
             
-        } )
-        
+        } )}
+        </Card>
     )
 }
 
